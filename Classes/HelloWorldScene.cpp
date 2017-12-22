35faaee7885fc2d9510b7188bd416ad9fa1e155f
Lê Phương Khanh <khanhlp315@gmail.com>
2017-12-22 17:10:07 +0700

    Add ground to scene

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -25,30 +25,37 @@ bool HelloWorld::init()
         return false;
     }
 
-	auto testMap = TMXTiledMap::create("Maps/Test/Map0.tmx");
+	const auto testMap = TMXTiledMap::create("Maps/Level1.tmx");
 	this->addChild(testMap, 0);
 
-	auto characters = testMap->getObjectGroup("CharacterLayer");
+	const auto characters = testMap->getObjectGroup("PlayerObject");
 	auto player = characters->getObject("Player");
 
 	auto spriteTiny = Tiny::Tiny::create();
 	spriteTiny->setPosition(player.at("x").asFloat(), player.at("y").asFloat());
 	this->addChild(spriteTiny,1);
 
-	const auto grounds = testMap->getObjectGroup("GroundLayer");
-	auto ground = grounds->getObject("Ground0");
-	auto bodyGround = cocos2d::PhysicsBody::createBox(cocos2d::Size(158, 509), cocos2d::PHYSICSBODY_MATERIAL_DEFAULT);
+	const auto grounds = testMap->getObjectGroup("GroundObject");
 
-	bodyGround->getShape(0)->setRestitution(1.0f);
-	bodyGround->getShape(0)->setFriction(0.0f);
-	bodyGround->getShape(0)->setDensity(1.0f);
-	bodyGround->setDynamic(false);
+	for(auto ground: grounds->getObjects())
+	{
+		auto groundNode = Node::create();
 
-	auto groundNode = Node::create();
-	groundNode->setPhysicsBody(bodyGround);
-	groundNode->setPosition(1 + 158/2, 499 - 509/2);
+		auto groundValues = ground.asValueMap();
 
-	this->addChild(groundNode);
+		auto bodyGround = PhysicsBody::createBox(Size(groundValues.at("width").asFloat(), groundValues.at("height").asFloat()), PHYSICSBODY_MATERIAL_DEFAULT);
+
+		bodyGround->getShape(0)->setRestitution(0.0f);
+		bodyGround->getShape(0)->setFriction(0.0f);
+		bodyGround->getShape(0)->setDensity(1.0f);
+		bodyGround->setRotationEnable(false);
+		bodyGround->setDynamic(false);
+
+		groundNode->setPhysicsBody(bodyGround);
+		bodyGround->setContactTestBitmask(0x000001);
+		groundNode->setPosition(groundValues.at("x").asFloat() + groundValues.at("width").asFloat() / 2, groundValues.at("y").asFloat() + groundValues.at("height").asFloat() / 2);
+		this->addChild(groundNode);
+	}
 
     return true;
 }
